700e7b9de3d016f9fd37157f03da86ee8ce6b54e
Marcus Medin <medina.jobb@outlook.com>
2022-08-15 17:11:46 +0200

    Aug 15 livecode

---
+++ Livekod/BGU20220815 - Hundår/Hundår/Program.cs
@@ -0,0 +1,18 @@
+﻿// Syfte: användarens ålder omvandlas till hundår
+
+// deklarera / definiera
+int dogYear = 0;
+int humanYear = 0;
+
+// Hämta data
+Console.WriteLine("Ange hundår");
+string age = Console.ReadLine();
+dogYear = int.Parse(age);
+
+// kontrollera 
+
+// beräkna
+humanYear = dogYear * 7;
+
+// presentera
+Console.WriteLine($"En hund som är {dogYear} år är {humanYear} år i människoålder");
---
+++ Livekod/BGU20220815 - MjölkExempel/MjölkExempel/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Gå till butiken, handla 1 paket mjök. Om det finns ägg handla 6 st");
+
+//Mjölk = 1
+int mjölk = 1;
+bool eggs = false;
+
+//Om Ägg finns 
+//Mjölk = 6
+if (eggs == true)
+    mjölk = 6;
+else
+    mjölk = 1;
+
+//Handla mjölk
+Console.WriteLine($"Handlar {mjölk}st paket mjölk");
+//Gå hem
